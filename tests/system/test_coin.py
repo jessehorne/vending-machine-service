56e9c03da187b8fab21b51d1a718f3efe3c98866
Jesse Horne <jesse.horne@pioneer.com>
2017-04-16 20:34:49 -0500

    shows list of coins versus 'INSERT COINS' if no coins exist

--- tests/system/test_coin.py
+++ tests/system/test_coin.py
@@ -14,10 +14,27 @@ def test_insert_coins__valid_coins():
         if not r.status_code == 200:
             assert False
 
-        r = requests.delete(ADDRESS + "/coin")
+    r = requests.delete(ADDRESS + "/coin")
 
 
 def test_display__no_coins():
     r = requests.get(ADDRESS + "/coin")
 
     assert "INSERT COINS" in r.text
+
+
+def test_display__existing_coin():
+    coins = ["nickle", "dime", "quarter"]
+
+    for coin in coins:
+        r = requests.post(ADDRESS + "/coin/{}".format(coin))
+
+        if not r.status_code == 200:
+            assert False
+
+
+    r = requests.get(ADDRESS + "/coin")
+
+    assert "INSERT COINS" not in r.text
+
+    r = requests.delete(ADDRESS + "/coin")
