fa2c735a901ca74a743f2f9fbc41ef81a2c699bc
Jesse Horne <j.horne2796@gmail.com>
2017-04-26 14:23:54 -0500

    added tests for the purchasing of products

---
+++ tests/system/test_product.py
@@ -0,0 +1,16 @@
+import requests
+
+
+ADDRESS = "http://localhost:5000"
+
+
+def test_buy_product__exact_change():
+    assert False
+
+
+def test_buy_product__invalid_change():
+    assert False
+
+
+def test_buy_product__more_change():
+    assert False
